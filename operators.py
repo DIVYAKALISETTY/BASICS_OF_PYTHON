e1a43a12c7884735f9c194e5a70f2d867193cda6
DIVYAKALISETTY <134999556+DIVYAKALISETTY@users.noreply.github.com>
2025-04-21 13:18:27 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,35 @@
+#operators in python used to perform operations between the values
+#1 Arthmetic operator(+,-,*,/,%,//)
+#2. Assignment operator(=)
+#3.comparision opeartor(<,>,<=,>=,==,!=)
+#4. Logical opeartor
+#--------------------------------------------------------------------
+# 1 Arthmetic opeartor(+,-,*,/,%,//)
+x=4
+y=2
+print(x+y)#---->addition
+print(x-y)#----->Sustratction
+print(x*y)#------>multiplication
+print(x/y)#---->divison---->retrn decimal queotant
+print(x//y)#---->floor divsion/integerdivion------return queotant
+print(x%y)#------>modulo opeator---->return remainder
+# Assignment operator(=)
+x1=5
+print(x1)
+# here x --->variable
+#=---> opeartor
+#5---->value
+#comparision opeartors(<,>,<=,>=,==,!=)
+print(x<y)
+print(x>y)
+print(x<=y)
+print(x>=y)
+print(x==y)
+print(x!=y)
+#logical operator(and,or)
+print(x and y)
+print(x or y)
+#bitwise operator
+print(x & y)
+print(x|y)
+
