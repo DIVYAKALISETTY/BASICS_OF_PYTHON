f1a765807b8445a8a63efcba1b261329a7641db3
DIVYAKALISETTY <134999556+DIVYAKALISETTY@users.noreply.github.com>
2025-04-21 16:57:32 +0530

    Add files via upload

---
+++ Advanced_Datatypes_List.py
@@ -0,0 +1,25 @@
+#list--->which is used to store sequence of heterogeneous elements  in square brackets"[]"
+#create a list
+list1=[10,"divya",20,"ratna",30,"jyothi"]#------->storing heterogeneous elements
+list2=[1,2,3,4,5,6]#------->,strong homogeneous elements
+print(list1)
+print(type(list1))#-->type()-->function is used to know the type of the object
+print(len(list1))#----->len()-->used to find the length of the string
+print(list1[::-1])
+print(list2[::-2])
+#modifying element in a list
+list1[0]="anjali"
+print(list1)
+list1[1:4]=1,2,3
+print(list1)
+print(list1[0]*list1[3])
+#del list
+del list1
+print(list2)
+list2.clear()
+print(list2)
+#create 2 list one with homogeneous elements and another with heterogeneous elements
+#and concatenate both list
+l1=[1,2,3,4]
+l2=[1,"ammu",2.0,3+5j,True]
+print(l1+l2*3)
